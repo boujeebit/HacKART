9af4b50c43553c87e9b1bcfb90977dd2822d7adf
Mickey <14190058+boujeebit@users.noreply.github.com>
2023-04-10 18:33:19 -0500

    Enforcing use of environment to hide secrets

--- server/backend/core/settings.py
+++ server/backend/core/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,13 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b0=0jme=rk+t0*xva1r%d6(rgr4di#b!)4hbq+dbje7dmkvq5d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get("DEBUGGER") == 'True':
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = os.environ.get('DEBUGGER', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
